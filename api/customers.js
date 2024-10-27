5f175f70b413d152eddc8e3044cfc7035fa9fd41
Ludovico Colucci <ludovicocolucci@gmail.com>
2024-10-27 18:03:35 +0100

    [REF] routes

--- api/customers.js
+++ api/customers.js
@@ -13,16 +13,21 @@ router.get("/", async (req, res) => {
   }
 });
 
-// Rotta per ottenere un cliente specifico per ID
+// Rotta per ottenere un singolo cliente tramite ID
 router.get("/:id", async (req, res) => {
-  const { id } = req.params;
+  const { id } = req.params; // Estrai l'ID dai parametri della richiesta
   try {
+    // Esegui la query sul database per ottenere il cliente con l'ID specifico
     const result = await pool.query("SELECT * FROM customers WHERE id = $1", [
       id,
     ]);
+
+    // Controlla se il cliente esiste
     if (result.rows.length === 0) {
       return res.status(404).send("Cliente non trovato");
     }
+
+    // Invia il cliente come risposta
     res.status(200).json(result.rows[0]);
   } catch (err) {
     console.error("Errore nel server:", err.message);
